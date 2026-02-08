4822b7cd97397e92c541dc7d87fff305ab19ccd2
Nikita Moshenskiy <nikita@moshensky.com>
2026-02-08 20:50:02 +0200

    feat: add storybook basic example (#2)
    * add basic storybook example proj
    
    * disable telemetry
    
    * no open

---
+++ examples/storybook-basic/.storybook/main.ts
@@ -0,0 +1,13 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "core": {
+    "disableTelemetry": true
+  },
+  "addons": [],
+  "framework": "@storybook/react-vite"
+};
+export default config;
